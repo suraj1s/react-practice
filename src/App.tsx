63c2e4d0c73e5aea8c6e4b13c708e7f7809c8a14
suraj subedi <surajsubedi059@gmail.com>
2024-04-12 16:44:51 +0545

    project init

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+function App() {
+  return <body className="min-h-screen min-w-screen bg-slate-950 flex items-center justify-center">
+    <canvas className="border-2 border-slate-300 rounded-md w-[90%] h-[90%]" />
+  </body>
+}
+export default App;
