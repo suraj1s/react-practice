c00727b4d6f922c3ef3283e819d6eadbe338425d
suraj subedi <surajsubedi059@gmail.com>
2024-04-14 14:04:39 +0545

    feat: animate circle

--- canvas/src/App.tsx
+++ canvas/src/App.tsx
@@ -31,43 +31,119 @@ const App = () => {
 
   useEffect(() => {
     if (!canvasRef.current) return;
-    canvasRef.current.width = window.innerWidth * 0.8;
-    canvasRef.current.height = window.innerHeight * 0.8;
+    canvasRef.current.width = window.innerWidth ;
+    canvasRef.current.height = window.innerHeight;
   }, []);
 
+  const handelAnimation = ({
+    e,
+    randomHexColor,
+  }: {
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>;
+    randomHexColor: string;
+  }) => {
+    const canvas2d = e.currentTarget.getContext("2d");
+    const radius = 30;
+    if (!canvas2d) return;
+
+    // console.log("clientX", e.clientX, "offsetLeft", e.currentTarget.offsetLeft);
+    // console.log("clientY", e.clientY, "offsetTop", e.currentTarget.offsetTop);
+
+    let x = e.clientX - e.currentTarget.offsetLeft;
+    let y = e.clientY - e.currentTarget.offsetTop;
+
+    let dx =10; // this is the speed of the circle in x direction
+    let dy =10; // this is the speed of the circle in y direction
+
+    const animate = () => {
+      canvas2d.clearRect(0, 0, innerWidth, innerHeight);
+      // rectangle
+      canvas2d.fillRect(x, y, 50, 50);
+      canvas2d.fillStyle = randomHexColor;
+
+      // circle
+      canvas2d.beginPath();
+      canvas2d.arc(x, y, radius, 0, Math.PI * 2);
+      canvas2d.strokeStyle = randomHexColor;
+      canvas2d.stroke();
+      // console.log("animating...", x, y);
+      if(x + radius > innerWidth || x - radius < 0){
+        dx = -dx;
+      }
+      if(y + radius > innerHeight || y - radius < 0){
+        dy = -dy;
+      }
+      x += dx;  
+      y += dy;    
+      // requestAnimationFrame(animate); 
+    };
+    animate();
+  };
   return (
     <body className="min-h-screen min-w-screen  bg-slate-100  flex justify-center items-center gap-5">
       <canvas
         onClick={(e) => {
-          const canvas2d = e.currentTarget.getContext("2d");
-          if (!canvas2d) return;
-          // console.log("clientX", e.clientX, "offsetLeft", e.currentTarget.offsetLeft);
-          // console.log("clientY", e.clientY, "offsetTop", e.currentTarget.offsetTop);
-
-          const x = e.clientX - e.currentTarget.offsetLeft;
-          const y = e.clientY - e.currentTarget.offsetTop;
-
-          const rendomHexColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-          
-          canvas2d.fillRect(x, y, 50, 50);
-          canvas2d.fillStyle = rendomHexColor;
-          // circle
-          canvas2d.beginPath();
-          canvas2d.arc(x, y, 30, 0, 7);
-          canvas2d.strokeStyle = rendomHexColor
-          canvas2d.stroke();
+          const randomHexColor = `#${Math.floor(
+            Math.random() * 16777215
+          ).toString(16)}`;
+          handelAnimation({ e, randomHexColor });
         }}
         ref={canvasRef}
         className="border-2  border-slate-300 rounded-md"
       />
-      {/* <button onClick={()=> {
-        const randomX = Math.floor(Math.random() * 200);
-        const randomY = Math.floor(Math.random() * 200);
-        canvas2d?.fillRect(randomX, randomY, 20, 40);
-      }}
-      className="p-20"
-      >Fill rect</button> */}
     </body>
   );
 };
 export default App;
+
+// const canvas2d = e.currentTarget.getContext("2d");
+// if (!canvas2d) return;
+// console.log(
+//   "clientX",
+//   e.clientX,
+//   "offsetLeft",
+//   e.currentTarget.offsetLeft
+// );
+// console.log(
+//   "clientY",
+//   e.clientY,
+//   "offsetTop",
+//   e.currentTarget.offsetTop
+// );
+// let x = e.clientX - e.currentTarget.offsetLeft;
+// let y = e.clientY - e.currentTarget.offsetTop;
+
+// // handelAnimation({ event: e, randomHexColor });
+
+// const animate = () => {
+//   canvas2d.clearRect(0, 0, window.innerWidth, window.innerHeight);
+//   // rectangle
+//   canvas2d.fillRect(x, y, 50, 50);
+//   canvas2d.fillStyle = randomHexColor;
+
+//   // circle
+//   canvas2d.beginPath();
+//   canvas2d.arc(x, y, 30, 0, Math.PI * 2);
+//   canvas2d.strokeStyle = randomHexColor;
+//   canvas2d.stroke();
+//   console.log("animating...", x, y);
+//   // requestAnimationFrame(animate);
+//   if (!canvasRef.current) return;
+//   console.log(
+//     "clientX",
+//     e.clientX,
+//     "offsetLeft",
+//     canvasRef.current.offsetLeft
+//   );
+//   console.log(
+//     "clientY",
+//     e.clientY,
+//     "offsetTop",
+//     canvasRef.current.offsetTop
+//   );
+//   if (x < e.clientX + canvasRef.current.offsetLeft) x += 1;
+//   if (y < e.clientY + canvasRef.current.offsetTop) y += 1;
+//   if (x > e.clientX + canvasRef.current.offsetWidth) x -= 1;
+//   if (y > e.clientY + canvasRef.current.offsetHeight) y -= 1;
+// };
+// animate();
