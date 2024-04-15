4b797422df1379f6ab2424bf5c4248920571f4eb
suraj subedi <surajsubedi059@gmail.com>
2024-04-15 16:45:01 +0545

    feat: resizing rectangle

--- canvas/src/utils/canvas/rectangle.ts
+++ canvas/src/utils/canvas/rectangle.ts
@@ -1,11 +1,17 @@
 class Rectangle {
   x: number;
   y: number;
-  canvas2d: CanvasRenderingContext2D
+  canvas2d: CanvasRenderingContext2D;
   width: number;
   height: number;
 
-  constructor(canvas2d: CanvasRenderingContext2D, x: number, y: number, width: number, height: number) {
+  constructor(
+    canvas2d: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
     this.canvas2d = canvas2d;
     this.x = x;
     this.y = y;
@@ -15,19 +21,23 @@ class Rectangle {
   }
 
   draw() {
-   this.canvas2d.fillRect(this.x, this.y, this.width, this.height);
+    this.canvas2d.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    this.canvas2d.strokeRect(this.x, this.y, this.width, this.height)
   }
 
-  // for editing the rectangle i.e resizing from all sides and corners
-  updateSideRight = (mouseMoveX: number) => {
-    this.width = mouseMoveX - this.x;
-  };
-//   updateSideLeft = (mouseMoveX: number) => {
-//     this.width = this.x - mouseMoveX;
-//     this.x = mouseMoveX;
-//   };
-updateSideBottom = (mouseMoveY: number) => {
-    this.height = mouseMoveY - this.y;
+  update(x?: number, y?: number) {
+    if(x) this.width = x - this.x;
+    if(y) this.height = y - this.y;
+    this.draw();
+  }
+
+  isIntersecting(x: number, y: number) {
+    const isIntersectingLeft = x > this.x - 5 && x < this.x + 5;
+    const isIntersectingRight = x > this.x + this.width - 5 && x < this.x + this.width + 5;
+    return {
+      isIntersectingLeft,
+      isIntersectingRight
+    }
   }
 }
 
